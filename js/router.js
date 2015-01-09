8abe4a7c1d34d82c43f7bf17f1b9a7697423fd2a
Ankit gupta <ankit.gupta8898@gmail.com>
2015-01-09 15:13:07 -0500

    Controller and model updates with fixtures

--- js/router.js
+++ js/router.js
@@ -1,3 +1,12 @@
 Todos.Router.map(function(){
   this.resource('todos',{path: '/'});
+  //this.route('todos');
 });
+
+
+Todos.TodosRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.find('todo');
+  }
+});
+
